6c84946d92fffdd0dcdda2b2d614ece3cafe33e6
Alexander Lulkin <a.lulkin@gmail.com>
2023-09-19 08:17:22 +0200

    some plotting scripts

---
+++ test/data_to_mat.jl
@@ -0,0 +1,79 @@
+using MAT
+using Amica
+using SignalAnalysis
+using LinearAlgebra
+#using PyMNE
+
+#_____________________________________________
+#Generate multiple sin mixed with pink gaussian
+m = 3
+M = 2
+n = 4 #at least 4 to fit all sinusses
+N = 1000
+
+t = range(0,20*π,length=N)
+s =rand(PinkGaussian(length(t)),n)'
+s[2,:] = sin.(t)
+s[3,:] = sin.(2 .* t)
+s[4,:] = sin.(10 .* t)
+#s = s .* [1,2,3,4]
+A = rand(size(s,1),size(s,1))
+A2 = rand(size(s,1),size(s,1))
+#A = [1 1 0 0; 0 1 1 0; 0 0 1 1; 1 0 1 0]
+Id =1* Matrix(I, n, n) 
+
+x = zeros(n,N)
+s = Id*s #for some reason s doesnt get saved as a matrix otherwise...
+x[:,1:500] = A*s[:,1:500]
+x[:,500:1000] = A2*s[:,500:1000]
+
+#just for testing
+x_unmixed = zeros(n,N)
+x_unmixed[:,1:500] = pinv(A)*x[:,1:500]
+x_unmixed[:,500:1000] = pinv(A2)*x[:,500:1000]
+
+#_____________________________________________
+#_____________________________________________
+#get eeg data
+# data_path = pyconvert(String,@py(str(PyMNE.datasets.ssvep.data_path())))
+# bids_fname =  joinpath(data_path,"sub-02","ses-01","eeg","sub-02_ses-01_task-ssvep_eeg.vhdr")
+
+
+# raw = PyMNE.io.read_raw_brainvision(bids_fname, preload=true, verbose=false)
+# raw.resample(128)
+# raw.filter(l_freq=1, h_freq=nothing, fir_design="firwin")
+# x = pyconvert(Array,raw.get_data(;units="uV"))
+#_____________________________________________
+
+#initialise random parameters (A, beta, mu) before saving them
+beta = ones(m, n, M) + 0.1 * randn(m, n, M)
+
+if m > 1
+    mu = 0.1 * randn(m, n, M)
+else
+    mu = zeros(m, n, M)
+end
+
+eye = Matrix{Float64}(I, n, n)
+A_init = zeros(n,n,M)
+for h in 1:M
+    A_init[:,:,h] = eye[n] .+ 0.1*rand(n,n)
+    for i in 1:n
+        A_init[:,i,h] = A_init[:,i,h] / norm(A_init[:,i,h])
+    end
+end
+
+file = matopen("test/blabla.mat", "w")
+write(file, "x", x)
+write(file, "s", s) #only save for self-mixed data
+write(file, "A", A) # "
+write(file, "A2", A2) # "
+
+write(file, "beta_init", beta)
+write(file, "A_init", A_init)
+write(file, "mu_init", mu)
+close(file)
+
+# file = matopen("pink_sinus_data.mat")
+# x = read(file, "data")
+# close(file)
---
+++ test/run_amica.jl
@@ -0,0 +1,101 @@
+import Amica
+using Amica
+#using CairoMakie
+using MAT
+using LinearAlgebra
+
+# #Old hardcoded stuff
+# data = [1 4; 4 1]*Float64.([1.0 2 3; 4 5 6])
+# M = 2 #number of mixture models
+# m = 3 #number of source density mixtures
+
+
+# A = zeros(size(data,1),size(data,1),M)
+# A[:,:,1] = [1.0 0.003; -0.05 1.0]
+# A[:,:,2] = [2.0 0.003; -0.05 1.0]
+
+# beta = ones(m, size(data,1), M)
+# beta[:,:,1] = [1.1 0.9; 1.0 0.9; 0.9 0.8]
+# beta[:,:,2] = [1.2 0.9; 1.1 0.8; 0.9 0.7]
+
+# mu = zeros(m, size(data,1), M)
+# mu[:,:,1] = [0.1 0.9; -0.01 0.0; 0.0 -0.02] #todo: wieder rnd mu einfügen
+# mu[:,:,2] = [0.2 1; -0.01 0.0; 0.0 -0.03]
+
+# amica = MultiModelAmica(Amica.removeMean!(data); M=2, maxiter=4, A=A ,mu=mu, beta=beta)
+# Amica.amica!(amica, data; mindll=1e-8, iterwin=1)
+
+#fit(amica, x, )
+
+
+# z, A, Lt, LL = amica(data, M, m, maxiter, update_rho, mindll, iterwin, do_newton, remove_mean)
+
+#___________________________________________________________________________________
+#Sinus data from mat file
+#file = matopen("test/eeg_data.mat")
+file = matopen("test/sinus_multimodel_data.mat")
+x = read(file, "x")
+#s = read(file, "s")
+#A = read(file, "A")
+
+beta_init = read(file, "beta_init")
+mu_init = read(file, "mu_init")
+A_init = read(file, "A_init")
+
+close(file)
+#@time am = fit(SingleModelAmica,x;maxiter=1000, m=3, remove_mean = true,do_sphering = false, beta=beta_init[:,:,1], mu=mu_init[:,:,1], A=copy(A_init[:,:,1]))
+@time am = fit(MultiModelAmica,x;maxiter=1000, M = 2, iterwin = 50, m=3, remove_mean = false,do_sphering = false, beta=beta_init, mu=mu_init, A=copy(A_init))
+#@time am = fit(SingleModelAmica,x;maxiter=538, m=3)
+#@time am = fit(MultiModelAmica,x;maxiter=50,M=2, m=3)
+
+#W = pinv(am.A[:,:,1]) #previously [:,:,2]
+
+#---
+# f = Figure()
+# #series(f[1,1],s[:,1:100])
+# #ax,h = heatmap(f[1,2],A)
+# #Colorbar(f[1,3],h)
+
+# series(f[2,1],x[:,1:100])
+# #ax,h = heatmap(f[2,2],am.A[:,:,1])
+# #Colorbar(f[2,3],h)
+
+# series(f[3,1],pinv(am.A[:,:,1])*x[:,1:100])
+# #series(f[4,1],am.Lt)
+# series(f[4,1],am.LL)
+
+# series(f[4,2],(W*x)[:,1:100])
+
+# f
+# # series(f[4,1],pinv(W)'*x)
+# # series(f[6,1],W'*x)
+
+#----
+
+#_____________________________________________________________
+# #use eeg from .mat
+# file = matopen("test/eeg_data.mat")
+# x = read(file, "x")
+
+# beta_init = read(file, "beta_init")
+# mu_init = read(file, "mu_init")
+# A_init = read(file, "A_init")
+
+# close(file)
+
+# am = fit(MultiModelAmica,x;maxiter=20,M=1, m=3, beta=beta_init, mu=mu_init, A=A_init)
+
+#plot
+# size(am.A)
+# W = pinv(am.A[:,:,1]) #previously [:,:,2]
+
+# f = Figure()
+# series(f[1,1],d[1:7,1:100])
+
+# ax,h = heatmap(f[1,2],am.A[:,:,1])
+# Colorbar(f[1,3],h)
+
+# unmixed_d = am.A[:,:,1]*d
+# series(f[3,1],pinv(unmixed_d[1:7,1:100]))
+# series(f[4,1],am.Lt)
+# series(f[4,2],am.LL)
